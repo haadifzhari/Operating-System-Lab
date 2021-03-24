e17ae03229fc4506f285dfe50a9903ce5ebfa283
AfiqAmmar <afiqammar0000@gmail.com>
2021-03-24 15:33:21 +0800

    "Change to function"

--- Lab 1/BestFit.py
+++ Lab 1/BestFit.py
@@ -1,38 +1,42 @@
 memory = [50, 30, 700, 200, 900]
 process = [20, 200, 500, 50]
+a = len(memory)
+b = len(process)
   
-block = []
-for x in range(0, len(memory)): 
-    block.append(-1)
+def bestFit(memorySize, processSize, a, b):
+    block = []
+    for x in range(0, a): 
+        block.append(-1)
 
-print("Block Number \t Memory Size")  
-for z in range(0, len(memory)):
-    print(z, "\t\t", memory[z])  
-print()
+    print("Block Number \t Memory Size")  
+    for z in range(0, a):
+        print(z, "\t\t", memorySize[z])  
+    print()
  
-for y in range(0, len(process)): 
-    for z in range(0, len(memory)): 
-        if process[y] <= memory[z]:
-            block[y] = z
-            count = 0
-            for f in range(0, len(memory)):
-                if memory[z] > memory[f] and process[y] <= memory[f]:
-                    block[y] = f
-                    memory[f] = -1
-                    count = 1
-                    break
-            if count == 0:
-                memory[z] = -1
-            break
+    for y in range(0, b): 
+        for z in range(0, a): 
+            if processSize[y] <= memorySize[z]:
+                block[y] = z
+                count = 0
+                for f in range(0, a):
+                    if memorySize[z] > memorySize[f] and processSize[y] <= memorySize[f]:
+                        block[y] = f
+                        memorySize[f] = -1
+                        count = 1
+                        break
+                if count == 0:
+                    memorySize[z] = -1
+                break
 
-print("Jobs \t Size \t Block number") 
-for y in range(0, len(process)):
-    print((y+1), "\t", process[y], end="\t\t")
-    if block[y] != -1:   
-        print(block[y])
-    else:
-        print("Blocks not allocated")
+    print("Jobs \t Size \t Block number") 
+    for y in range(0, b):
+        print((y+1), "\t", processSize[y], end="\t\t")
+        if block[y] != -1:   
+            print(block[y])
+        else:
+            print("Blocks not allocated")
 
-        
+bestFit(memory, process, a, b)        
 
+ 
         
--- Lab 1/FirstFit.py
+++ Lab 1/FirstFit.py
@@ -1,28 +1,33 @@
 memory = [222, 100, 700, 400, 40]
 process = [60, 75, 175, 275, 800]
+a = len(memory)
+b = len(process)
 
-block = []
-for x in range(0, len(memory)): 
-    block.append(-1)
-    
-print("Block Number \t Memory Size") 
-for z in range(0, len(memory)):
-    print(z, "\t\t", memory[z]) 
-print()
+def firstFit(memorySize, processSize, a, b):
+    block = []
+    for x in range(0, a): 
+        block.append(-1)
+
+    print("Block Number \t Memory Size")  
+    for z in range(0, a):
+        print(z, "\t\t", memorySize[z])  
+    print()
  
-for y in range(0, len(process)):  
-    for z in range(0, len(memory)): 
-        if process[y] <= memory[z]: 
-            block[y] = z
-            memory[z] = -1
-            break
+    for y in range(0, b): 
+        for z in range(0, a): 
+            if processSize[y] <= memorySize[z]:
+                block[y] = z
+                memorySize[z] = -1
+                break
 
-print("Jobs \t Size \t Block number") 
-for y in range(0, len(process)):
-    print((y+1), "\t", process[y], end="\t\t")
-    if block[y] != -1:       
-        print(block[y])
-    else:
-        print("Blocks not allocated")
+    print("Jobs \t Size \t Block number") 
+    for y in range(0, b):
+        print((y+1), "\t", processSize[y], end="\t\t")
+        if block[y] != -1:   
+            print(block[y])
+        else:
+            print("Blocks not allocated")
+
+firstFit(memory, process, a, b) 
     
 
